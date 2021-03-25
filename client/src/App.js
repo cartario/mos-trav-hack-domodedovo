bf6f78d47e823bc1259f8b72589b35350b9c6d45
VasiliyMac <cartario@yandex.ru>
2021-03-25 13:04:56 +0300

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Hello2
+          Learn React - Hello3
         </a>
       </header>
     </div>
